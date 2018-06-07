25d365d0e2f58ed933c431f46d5a4feaf63f91f3
HassanMukhtar <38445589+hassmms@users.noreply.github.com>
2018-06-08 01:07:01 +1000

    Add files via upload

---
+++ Python-Selenium unittest/EpilepsyTC_UsingWheelchair.py
@@ -0,0 +1,199 @@
+import unittest
+import time
+import HtmlTestRunner
+import os
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+cwd = os.getcwd()+'/results' 
+
+
+class Test_setUp_menu_buttons(unittest.TestCase):   
+    
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+
+    def tearDown(self):
+        self.driver.close()
+        
+    #@unittest.skip("skip")
+    def test_TC_UsingWheelchair01_iconClick(self):
+        driver = self.driver
+        driver.get("https://www.dfiinfo.com.au/beta-2-0-3d/")
+        time.sleep(2)
+        driver.find_element_by_xpath("//div[@id='firsttime']/div[2]/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='wrapper']/div[8]/div/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select").click()
+        Select(driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select")).select_by_visible_text("Epilepsy")
+        driver.find_element_by_xpath("//img[@alt='setup icon']").click()
+        driver.find_element_by_xpath("//img[@alt='Walking']").click()
+        driver.find_element_by_xpath("//img[@alt='Wheelchair']").click()
+        time.sleep(2)
+        self.assertTrue(driver.find_element_by_id("wchair").is_selected())
+        
+        driver.save_screenshot('SC_TC_UsingWheelchair01_iconClick.png')
+        
+        
+    #@unittest.skip("skip")    
+    def test_TC_UsingWheelchair01_checkboxClick(self):
+        driver = self.driver
+        driver.get("https://www.dfiinfo.com.au/beta-2-0-3d/")
+        time.sleep(2)
+        driver.find_element_by_xpath("//div[@id='firsttime']/div[2]/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='wrapper']/div[8]/div/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select").click()
+        Select(driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select")).select_by_visible_text("Epilepsy")
+        driver.find_element_by_xpath("//img[@alt='setup icon']").click()
+        driver.find_element_by_xpath("//img[@alt='Walking']").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li/label/span").click()      
+        time.sleep(2)
+        self.assertTrue(driver.find_element_by_id("wchair").is_selected())
+        driver.save_screenshot('SC_TC_UsingWheelchair01_checkboxClick.png')
+        
+        
+    #@unittest.skip("skip")
+    def test_TC_UsingWheelchair01_textClick(self):
+        driver = self.driver
+        driver.get("https://www.dfiinfo.com.au/beta-2-0-3d/")
+        time.sleep(2)
+        driver.find_element_by_xpath("//div[@id='firsttime']/div[2]/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='wrapper']/div[8]/div/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select").click()
+        Select(driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select")).select_by_visible_text("Epilepsy")
+        driver.find_element_by_xpath("//img[@alt='setup icon']").click()
+        driver.find_element_by_xpath("//img[@alt='Walking']").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li/label").click()
+        time.sleep(2)
+        self.assertTrue(driver.find_element_by_id("wchair").is_selected())
+        driver.save_screenshot('SC_TC_UsingWheelchair01_textClick.png')
+        
+        
+    #@unittest.skip("skip")    
+    def test_TC_UsingWheelchair01_boxAreaClick(self):
+        driver = self.driver
+        driver.get("https://www.dfiinfo.com.au/beta-2-0-3d/")
+        driver.find_element_by_xpath("//div[@id='firsttime']/div[2]/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='wrapper']/div[8]/div/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select").click()
+        Select(driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select")).select_by_visible_text("Epilepsy")
+        driver.find_element_by_xpath("//img[@alt='setup icon']").click()
+        driver.find_element_by_xpath("//img[@alt='Walking']").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li").click()
+        time.sleep(2)
+        self.assertTrue(driver.find_element_by_id("wchair").is_selected())
+        driver.save_screenshot('SC_TC_UsingWheelchair01_boxAreaClick.png')
+       
+        
+    #@unittest.skip("skip")
+    def test_TC_UsingWheelchair02a(self):
+        driver = self.driver
+        driver.get("https://www.dfiinfo.com.au/beta-2-0-3d/")
+        time.sleep(2)
+        driver.find_element_by_xpath("//div[@id='firsttime']/div[2]/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='wrapper']/div[8]/div/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select").click()
+        Select(driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select")).select_by_visible_text("Epilepsy")
+        driver.find_element_by_xpath("//img[@alt='setup icon']").click()
+        driver.find_element_by_xpath("//img[@alt='Walking']").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li/label").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li/label").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li/label/span").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li/label").click()
+        driver.find_element_by_xpath("//img[@alt='Wheelchair']").click()
+        time.sleep(2)
+        self.assertFalse(driver.find_element_by_id("wchair").is_selected())
+        driver.save_screenshot('SC_TC_UsingWheelchair02a_unselect.png')
+      
+        
+        
+        
+        
+    #@unittest.skip("skip")     
+    def test_TC_UsingWheelchair02b(self):
+        driver = self.driver
+        driver.get("https://www.dfiinfo.com.au/beta-2-0-3d/")
+        driver.find_element_by_xpath("//div[@id='firsttime']/div[2]/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='wrapper']/div[8]/div/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select").click()
+        Select(driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select")).select_by_visible_text("Epilepsy")
+        driver.find_element_by_xpath("//img[@alt='setup icon']").click()
+        driver.find_element_by_xpath("//img[@alt='Walking']").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li/label").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li/label/span").click()
+        driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li/label").click()
+        driver.find_element_by_xpath("//img[@alt='Wheelchair']").click()
+        time.sleep(2)
+        self.assertTrue(driver.find_element_by_id("wchair").is_selected())
+        driver.save_screenshot('SC_TC_UsingWheelchair02b_select.png')  
+        
+        
+    #@unittest.skip("skip")    
+    def test_TC_UsingWheelchair03(self):
+        driver = self.driver
+        driver.get("https://www.dfiinfo.com.au/beta-2-0-3d/")
+        driver.find_element_by_xpath("//div[@id='firsttime']/div[2]/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='wrapper']/div[8]/div/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select").click()
+        Select(driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select")).select_by_visible_text("Epilepsy")
+        driver.find_element_by_xpath("//img[@alt='setup icon']").click()
+        driver.find_element_by_xpath("//img[@alt='Walking']").click()
+        button = driver.find_element_by_xpath("//img[@alt='Wheelchair']")
+        ActionChains(driver).move_to_element(button).click_and_hold(button).pause(6).release().perform()  
+        time.sleep(2)
+        self.assertTrue(driver.find_element_by_id("wchair").is_selected())
+        # assertEqual()
+        driver.save_screenshot('SC_TC_UsingWheelchair03.png')
+        
+    #@unittest.skip("skip still working on it, drag mouse over button")    
+    def test_TC_UsingWheelchair04(self):
+        driver = self.driver
+        driver.get("https://www.dfiinfo.com.au/beta-2-0-3d/")
+        driver.find_element_by_xpath("//div[@id='firsttime']/div[2]/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='wrapper']/div[8]/div/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select").click()
+        Select(driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select")).select_by_visible_text("Epilepsy")
+        driver.find_element_by_xpath("//img[@alt='setup icon']").click()
+        driver.find_element_by_xpath("//img[@alt='Walking']").click()
+        driver.find_element_by_xpath("//img[@alt='Wheelchair']")
+        button = driver.find_element_by_xpath("//div[@id='setup2']/ul/div[14]/li/label")
+        ActionChains(driver).drag_and_drop_by_offset(button, 30, 10).perform()
+        time.sleep(2)
+        self.assertTrue(driver.find_element_by_id("wchair").is_selected())
+        # assertEqual()
+        driver.save_screenshot('SC_TC_UsingWheelchair04.png')
+        
+        
+    #@unittest.skip("skip")   
+    def test_TC_UsingWheelchair05(self):
+        driver = self.driver          
+        driver.get("https://www.dfiinfo.com.au/beta-2-0-3d/")
+        time.sleep(2)
+        driver.find_element_by_xpath("//div[@id='firsttime']/div[2]/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='wrapper']/div[8]/div/button/div[2]").click()
+        driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select").click()
+        Select(driver.find_element_by_xpath("//div[@id='setupContainer']/div[2]/select")).select_by_visible_text("Epilepsy")
+        driver.find_element_by_xpath("//img[@alt='setup icon']").click()
+        driver.find_element_by_xpath("//img[@alt='Walking']").click()
+        driver.find_element_by_xpath("//img[@alt='Wheelchair']").click()
+        driver.find_element_by_xpath("//div[@id='setupContainer']/div/button/div[2]").click()
+        time.sleep(2)
+        driver.find_element_by_id("inputname").click()
+        driver.find_element_by_id("inputname").clear()
+        driver.find_element_by_id("inputname").send_keys("TEST")
+        driver.find_element_by_id("inputsub").click()
+        driver.find_element_by_id("inputsub").clear()
+        driver.find_element_by_id("inputsub").send_keys("Mornington")
+        driver.find_element_by_id("inputsub").send_keys(Keys.ENTER)
+        time.sleep(2)
+        # assertEqual()
+        driver.save_screenshot('SC_TC_UsingWheelchair05.png') 
+        
+              
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testDFI_Search']
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output=cwd))
